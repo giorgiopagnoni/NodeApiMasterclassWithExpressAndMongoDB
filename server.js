10413b5c5042d893e6aba42c250e85383ce1f9d8
Giorgio Pagnoni <pag.giorgio@gmail.com>
2021-05-29 18:18:26 +0200

    prime rotte

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+const bootcamps = require('./routes/bootcamps');
+app.use('/api/v1/bootcamps', bootcamps);
+
 app.listen(PORT);
