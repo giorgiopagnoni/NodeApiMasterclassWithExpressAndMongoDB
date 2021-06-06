91509ef90d7d17a51624cebf4c803deb3c97be05
Giorgio Pagnoni <pag.giorgio@gmail.com>
2021-06-06 17:53:50 +0200

    pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     const reqQuery = {...req.query};
 
     // exclude fields
-    const removeFields = ['select', 'sort'];
+    const removeFields = ['select', 'sort', 'page', 'limit'];
     removeFields.forEach(param => delete reqQuery[param]);
 
     // create query operators ($gt, $lte...)
@@ -33,11 +33,29 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
         query = query.sort('-createdAt');
     }
 
+    // pagination
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Bootcamp.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
     const bootcamps = await query;
+
+    // pagination
+    const pagination = {};
+    if (endIndex < total) {
+        pagination.next = {page: page + 1, limit};
+    }
+    if (startIndex > 1) {
+        pagination.prev = {page: page - 1, limit};
+    }
 
     res.status(200).json({
         success: true,
         count: bootcamps.length,
+        pagination,
         data: bootcamps
     });
 });
