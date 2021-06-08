cda76a43f6b6c33f712a448e0c1558436b651fef
Giorgio Pagnoni <pag.giorgio@gmail.com>
2021-06-08 10:54:00 +0200

    course model and seeder

--- utils/seeder.js
+++ utils/seeder.js
@@ -5,12 +5,15 @@ const connectDB = require('./../config/db');
 dotenv.config({path: `${__dirname}/../.env`});
 
 const Bootcamp = require('./../models/Bootcamp');
+const Course = require('./../models/Course');
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/../_data/bootcamps.json`, 'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/../_data/courses.json`, 'utf-8'));
 
 const importData = async () => {
     try {
         await connectDB();
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
         console.log('data imported');
         process.exit();
     } catch (e) {
@@ -22,6 +25,7 @@ const deleteData = async () => {
     try {
         await connectDB();
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log('data deleted');
         process.exit();
     } catch (e) {
