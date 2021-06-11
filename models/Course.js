001b2a292e3506037f4ece822f30166244cdd5ce
Giorgio Pagnoni <pag.giorgio@gmail.com>
2021-06-11 15:39:41 +0200

    courses average cost

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,32 @@ const CourseSchema = new mongoose.Schema({
     }
 });
 
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    const obj = await this.aggregate([
+        {
+            $match: {bootcamp: bootcampId}
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: {$avg: '$tuition'}
+            }
+        }
+    ]);
+
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+        averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+    })
+};
+
+// average cost after save
+CourseSchema.post('save', function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
+// average cost after remove
+CourseSchema.pre('remove', function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model('Course', CourseSchema);
